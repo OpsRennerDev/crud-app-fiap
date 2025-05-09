31cb99bddf56190280e32aaed52ad6a9c31d2d03
Roberth Renner <roberth.renner12@gmail.com>
2025-05-08 21:02:18 -0300

    ajustando o nome do componente

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -2,7 +2,7 @@
 import { useState } from "react"
 import { useRouter } from "next/navigation"
 
-export default function login() {
+export default function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
